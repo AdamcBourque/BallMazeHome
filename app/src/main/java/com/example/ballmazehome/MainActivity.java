947660fb1a8d18f3957213a85168e6fc9de41f30
AdamcBourque <adamobourque@gmail.com>
2019-04-03 09:33:52 -0400

    added empty switch case for button click event

--- app/src/main/java/com/example/ballmazehome/MainActivity.java
+++ app/src/main/java/com/example/ballmazehome/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ballmazehome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button_click(View view){
+        switch (view.getId()){
+            case R.id.diffi:
+                break;
+            case R.id.high_scores:
+                break;
+            case R.id.skins:
+                break;
+            case R.id.info:
+                break;
+            default:
+                break;
+        }
     }
 }
