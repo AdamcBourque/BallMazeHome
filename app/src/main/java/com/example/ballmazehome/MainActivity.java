8e1c361fb9bd7599cca2d397086d089835c96146
ficti <adamobourque@gmail.com>
2019-04-04 20:00:52 -0400

    added email

--- app/src/main/java/com/example/ballmazehome/MainActivity.java
+++ app/src/main/java/com/example/ballmazehome/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ballmazehome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,13 +20,20 @@ public class MainActivity extends AppCompatActivity {
     public void button_click(View view){
         switch (view.getId()){
             case R.id.diffi:
-
+                Intent diffic = new Intent (this, difficulty_selector.class);
+                startActivity(diffic);
                 break;
             case R.id.high_scores:
+                Intent highsc = new Intent (this, high_score.class);
+                startActivity(highsc);
                 break;
             case R.id.skins:
+                Intent skin = new Intent (this, skins.class);
+                startActivity(skin);
                 break;
             case R.id.info:
+                Intent info = new Intent (this, info.class);
+                startActivity(info);
                 break;
             default:
                 break;
