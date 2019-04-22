0a75462fa6afcd806599c82fa8e851699ab599d7
Zach Masrer <zhess@vtc.vsc.edu>
2019-04-22 15:24:55 -0300

    Added Text viex and scroll for the information button.

--- app/src/main/java/com/example/ballmazehome/info.java
+++ app/src/main/java/com/example/ballmazehome/info.java
@@ -1,13 +1,21 @@
 package com.example.ballmazehome;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class info extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+
     }
 }
