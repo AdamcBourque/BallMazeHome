f432b35d91f2ce3d1f45936e20190c8218a41818
ficti <adamobourque@gmail.com>
2019-04-28 21:32:10 -0400

    Accelerometer created and "working"

--- app/src/main/java/com/example/ballmazehome/Maze.java
+++ app/src/main/java/com/example/ballmazehome/Maze.java
@@ -1,17 +1,88 @@
 package com.example.ballmazehome;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Maze extends AppCompatActivity  {
+public class Maze extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor accelerometer;
+    private SensorManager sensorManager;
+    private int currentSensor;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+    private ImageView ball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
-        MovingShit ball = new MovingShit(this);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        ball = findViewById(R.id.ball);
+        currentSensor = Sensor.TYPE_ACCELEROMETER;
+    }
+
+    public boolean checkSensorAvailability(int sensorType) {
+        boolean isSensor = false;
+        if (sensorManager.getDefaultSensor(sensorType) != null) {
+            isSensor = true;
+        }
+        return isSensor;
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == currentSensor) {
+            if (currentSensor == Sensor.TYPE_ACCELEROMETER) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                long curTime = System.currentTimeMillis();
+
+                if ((curTime - lastUpdate) > 100) {
+                    long diffTime = (curTime - lastUpdate);
+                    lastUpdate = curTime;
+
+                    float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                    last_x = x;
+                    last_y = y;
+                    last_z = z;
+                    ball.setLeft((ball.getLeft() + (int)speed));
+                    ball.setTop((ball.getTop() + (int)speed));
+
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
 }
 
 
