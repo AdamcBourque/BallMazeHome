3d2ab0c11b993b7b204272558004067a4672c767
ficti <adamobourque@gmail.com>
2019-05-02 20:09:57 -0400

    basic time variable created. collision detection method created (not functioning) Rect objects created and synced with ImageViews new ImageView goal created Zach told me to put this here Drink more Water

--- app/src/main/java/com/example/ballmazehome/Maze.java
+++ app/src/main/java/com/example/ballmazehome/Maze.java
@@ -1,24 +1,33 @@
 package com.example.ballmazehome;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+
+
+
 public class Maze extends AppCompatActivity implements SensorEventListener {
 
+    private long  startTime, stopTime, totalTime;
     private Sensor accelerometer;
     private SensorManager sensorManager;
     private int currentSensor;
     private long lastUpdate = 0;
     private float ballX = 30, ballY = 30;
-    private ImageView ball;
+    private ImageView ball_image, goal_image;
+    private Rect ball, goal;
+    Intent test = new Intent (this, high_score.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +35,27 @@ public class Maze extends AppCompatActivity implements SensorEventListener {
         setContentView(R.layout.activity_maze);
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        ball = findViewById(R.id.ball);
+        ball_image = findViewById(R.id.ball);
+        goal_image = findViewById(R.id.goal);
         currentSensor = Sensor.TYPE_ACCELEROMETER;
-        ballX = ball.getX();
-        ballY = ball.getY();
+        startTime = SystemClock.uptimeMillis();
+        // ballX = ball.getX();
+      //  ballY = ball.getY();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowmanager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        windowmanager.getDefaultDisplay().getMetrics(displayMetrics);
+        int deviceWidth = 5 * displayMetrics.widthPixels;
+        int deviceHeight = 10 * displayMetrics.heightPixels;
+        float xMax = deviceWidth / 6;
+        float yMax = deviceHeight / 13;
+        goal = new Rect();
+        ball = new Rect();
+        ballX = ball.left;
+        ballY = ball.top;
+        goal.set((int)(xMax - 80), (int)(yMax - 80), (int)xMax, (int)yMax);
+        ball.set((int)ballX, (int)ballY, (int)(ballX+80), (int)(ballY+80));
+        goal_image.setX(xMax - 80);
+        goal_image.setY(yMax - 80);
     }
 
     public boolean checkSensorAvailability(int sensorType) {
@@ -76,8 +102,14 @@ public class Maze extends AppCompatActivity implements SensorEventListener {
                     }else{
                         ballY = ballY + speedY;
                     }
-                    ball.setX(ballX);
-                    ball.setY(ballY);
+                    ball_image.setX(ballX);
+                    ball_image.setY(ballY);
+                    ball.offsetTo((int)ballX, (int)ballY);
+                    if (collides(goal)){
+                        stopTime = SystemClock.uptimeMillis();
+                        totalTime = stopTime - startTime;
+                        startActivity(test);
+                    }
                 }
             }
         }
@@ -99,6 +131,14 @@ public class Maze extends AppCompatActivity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    public boolean collides(Rect wall){
+        if (Rect.intersects(ball, wall)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
